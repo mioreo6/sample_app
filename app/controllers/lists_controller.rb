86d178387061b5b38322521c50a229b5e582ef31
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-04-10 02:28:33 +0000

    [Finish]3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,6 +24,12 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
   
+  def destroy
+    list = List.find(params[:id]) #データ（レコード）を１件取得
+    list.destroy #データ（レコード）を削除
+    redirect_to '/lists' #投稿画面一覧へリダイレクト
+  end
+  
   def update
     list = List.find(params[:id])
     list.update(list_params)
@@ -33,6 +39,6 @@ class ListsController < ApplicationController
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
